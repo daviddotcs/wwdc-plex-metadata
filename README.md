3d24f85501d12b460ae42f19c482dbb22405b065
David Wake <wake.david@gmail.com>
2016-07-01 23:22:09 +0800

    Improved scoring, added thumbnails
    Scoring now includes partial title matches. Thumbnails are now imported.

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ A Plex agent which provides metadata for [Apple Worldwide Developers Conference]
 
 ## Requirements
 * An installation of [Plex Media Server](https://www.plex.tv/).
+* A folder containing WWDC session videos.
 
 ## Installation
 1. Download or clone this repository and copy **WWDC.bundle** into your [Plex Plug-ins directory](https://support.plex.tv/hc/en-us/articles/201106098-How-do-I-find-the-Plug-Ins-folder-). E.g.;  
@@ -14,16 +15,20 @@ A Plex agent which provides metadata for [Apple Worldwide Developers Conference]
  | Windows  | %LOCALAPPDATA%\Plex Media Server\Plug-ins\ |
 
 2. In your Plex web interface visit **Settings > Server > Agents** and verify that you have a *WWDC* agent listed under *Movies*.
-3. Edit or create a new Movies or Home Vidoes library for a directory with WWDC session videos, and from the *Advanced* tab select the *WWDC* agent.
+3. Edit or create a new Movies or Home Videos library for a directory with WWDC session videos, and from the *Advanced* tab select the *WWDC* agent.
 
 ## File and Folder Structure
-This agent requires file names to include the 3 digit session number as the first number in the filename, and for the parent directory to be named including a 2 or 4 digit year number e.g.;
+For automatic matches, this agent requires the year and session number to be present in the filename or containing folder. The year may appear as a 2 or 4 digit number in either the parent directory or the start of the filename, and the session number must appear as the first 3+ digit, non-year number in the filename e.g.;
 ```
-/WWDC 2016/805_hd_iterative_ui_design.mp4
+/2016/805_hd_iterative_ui_design.mp4
+/wwdc16/805.mp4
+/2016-805.mp4
 ```
-The above example will be correctly parsed as session number 805 from WWDC 2016.
+The above examples will be correctly parsed as session number 805, *Iterative UI Design* from WWDC 2016.
 
 ## Limitations
-* Session thumbnails are not currently fetched.
-* Name matches are not implemented, so year and session numbers are crucial.
+* Tech Talks and archival Apple Developer videos are not supported.
+
+## License
+WWDC Plex Metadata Agent is copyright David Wake and available under a [MIT license](https://github.com/drmccheese/wwdc-plex-metadata/blob/master/LICENSE).
 
--- WWDC.bundle/Contents/Code/__init__.py
+++ WWDC.bundle/Contents/Code/__init__.py
@@ -4,6 +4,14 @@ from dateutil.parser import parse
 SOURCE_URL = "https://devimages-cdn.apple.com/wwdc-services/g7tk3guq/xhgbpyutb6wvn2xcrbcz/videos.json"
 DEFAULT_CACHE_TIME = 3600
 
+# Locate first 2 or 4 digit number as year (optional), and first 3+ digit number in filename as session id
+FILENAME_PARSE_RE = re.compile("^(?:(?:[\w\W]*\D)?(\d{2}|\d{4})\D)?(?:[\w\W]*\D)?(\d{3,})(?:\D[\w\W]*)?$")
+
+# Locate first 2 or 4 digit number in parent directory name as year
+PARENT_DIR_PARSE_RE = re.compile("^(?:[\w\W]*\D)?(\d{2}|\d{4})(?:\D[\w\W]*)?$")
+
+TITLE_CLEAN_RE = re.compile("([^a-z0-9 ]+)")
+
 ##################################################
 
 def Start():
@@ -19,27 +27,35 @@ class WwdcSession:
     self.description = None
     self.date = None
     self.categories = []
-    self.images = None
+    self.thumbnail = None
+    self.score = 0
 
   @staticmethod
   def fromFilename(path):
+      
+    def parseYear(s):
+      year = None
+      if s != None:
+        year = int(s)
+        if year < 100:
+          year += 2000
+      return year
+    
     session = WwdcSession()
 
     splitPath = os.path.split(path)
     filename = splitPath[1]
-    parent_directory = os.path.split(splitPath[0])[1]
 
-    # Locate first 3+ digit number in filename as session id
-    match = re.search("^(?:[\w\W]*\D)?(\d{3,})(?:\D[\w\W]*)?$", filename)
-    if match != None:
-      session.id = int(match.group(1))
+    match = FILENAME_PARSE_RE.search(filename)
+    if match:
+      session.year = parseYear(match.group(1))
+      session.id = int(match.group(2))
 
-    # Locate first 2 or 4 digit number in parent directory name as year
-    match = re.search("^(?:[\w\W]*\D)?(\d{2}|\d{4})(?:\D[\w\W]*)?$", parent_directory)
-    if match != None:
-      session.year = int(match.group(1))
-      if session.year < 100:
-        session.year = session.year + 2000
+    if session.year == None:
+      parent_directory = os.path.split(splitPath[0])[1]
+      match = PARENT_DIR_PARSE_RE.search(parent_directory)
+      if match:
+        session.year = parseYear(match.group(1))
 
     return session
 
@@ -50,7 +66,6 @@ class WwdcSession:
     session.id = int(sessionJson["id"])
     session.title = sessionJson["title"]
     session.description = sessionJson["description"]
-    session.images = sessionJson["images"]
 
     dateString = sessionJson["date"]
     if dateString != None and dateString != session.year:
@@ -66,6 +81,10 @@ class WwdcSession:
     focus = sessionJson["focus"]
     if isinstance(focus, collections.Sequence):
       session.categories.extend([x for x in focus if isinstance(x, str)])
+    
+    images = sessionJson["images"]
+    if "shelf" in images:
+      session.thumbnail = images["shelf"]
 
     return session
 
@@ -85,14 +104,47 @@ class WwdcSession:
 
 ##################################################
 
-def fetchSessions(year, id):
+def rankMatch(query, candidate):
+  if query == None or candidate == None:
+    return 0
+
+  queryWords = TITLE_CLEAN_RE.sub("", query.lower()).split(" ")
+  candidateWords = TITLE_CLEAN_RE.sub("", candidate.lower()).split(" ")
+
+  candidateLength = 0
+  matchLength = 0
+  for word in candidateWords:
+    candidateLength += len(word)
+    if word in queryWords:
+      matchLength += len(word)
+
+  if candidateLength == 0:
+    return 0
+
+  return float(matchLength) / candidateLength
+
+##################################################
+
+def fetchSessions(year, id, exact, name=None):
   result = []
 
   sessions = JSON.ObjectFromURL(SOURCE_URL, cacheTime=DEFAULT_CACHE_TIME)
 
   for sessionJson in sessions["sessions"]:
     session = WwdcSession.fromJson(sessionJson)
-    if (year == None or year == session.year) and (id == None or id == session.id):
+    score = 0
+
+    if year == session.year:
+      score += 30
+    
+    if id == session.id:
+      score += 70
+
+    if score < 100 and name != None:
+      score += int((100 - score) * rankMatch(name, session.title))
+
+    session.score = score
+    if (exact and year == session.year and id == session.id) or (not exact and score > 0):
       result.append(session)
 
   return result
@@ -119,17 +171,9 @@ class WwdcAgent(Agent.Movies):
     if session.year == None and session.id == None:
       return
 
-    # TODO: Enable fuzzy matching on session name and improve scoring
-    sessions = fetchSessions(session.year, session.id)
-
-    score = 100
-    if session.year == None:
-      score -= 20
-    if session.id == None:
-      score -= 50
-
+    sessions = fetchSessions(session.year, session.id, False, media.name)
     for session in sessions:
-      results.Append(MetadataSearchResult(id=session.getMetadataId(), name=session.title, year=session.year, score=score, lang=lang))
+      results.Append(MetadataSearchResult(id=session.getMetadataId(), name=session.title, year=session.year, score=session.score, lang=lang))
 
   def update(self, metadata, media, lang, force):
     session = WwdcSession.fromMetadataId(metadata.id)
@@ -137,14 +181,12 @@ class WwdcAgent(Agent.Movies):
     if session.year == None and session.id == None:
       return
 
-    sessions = fetchSessions(session.year, session.id)
+    sessions = fetchSessions(session.year, session.id, True)
 
     if len(sessions) != 1:
       return
 
     session = sessions[0]
-
-    # TODO: Use 'hero'/'playback'/'shelf' images
 
     if force or metadata.title == None or len(metadata.title) == 0:
       metadata.title = session.title
@@ -160,3 +202,9 @@ class WwdcAgent(Agent.Movies):
 
     if force or metadata.collections == None or len(metadata.collections) == 0:
       metadata.collections = session.categories
+
+    if session.thumbnail != None and session.thumbnail not in metadata.art:
+      try:
+        metadata.art[session.thumbnail] = Proxy.Media(HTTP.Request(session.thumbnail))
+      except:
+        pass
